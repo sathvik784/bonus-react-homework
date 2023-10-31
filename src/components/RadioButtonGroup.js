07b8672fbe4ef37735f345beb7c524d3f1981987
Aidan Melvin <aidmelvin.314@gmail.com>
2023-10-31 15:54:30 -0400

    updated for homework

--- src/components/RadioButtonGroup.js
+++ src/components/RadioButtonGroup.js
@@ -11,28 +11,16 @@ const RadioButtonGroup = () => {
 
 /* Don't worry about this function, changes color when a radio is clicked */
   const handleRadioButtonClick = (event) => {
-    setSelectedNumber(Number(event.target.value));
+    /* TODO: use the hook from line 10 to change the selectedNumber */
+    /* hint: you can get the new number with event.target.value */
   };
-/* Don't worry about this function, simply to pick the color */
+
   const getColor = () => {
     switch (selectedNumber) {
-      case 1:
-        return 'red';
-      case 2:
-        return 'blue';
-      case 3:
-        return 'green';
-      case 4:
-        return 'aqua';
-      case 5:
-        return 'purple';
-      default:
-        return 'black';
+      /* TODO: write logic to return the appropriate color */
     }
   };
-  /* What to do here
-
-  */
+  
   return (
     <div className="radio-button-group" style={{ backgroundColor: getColor() }}>
       {[1, 2, 3, 4, 5].map(number => (
